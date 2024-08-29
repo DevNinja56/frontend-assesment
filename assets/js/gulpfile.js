2d20b2deec408ad43b9d778cded8e6cbd4ad499e
DevNinja56 <danisamim9@gmail.com>
2024-08-29 11:08:59 +0500

    fix issues

--- assets/js/gulpfile.js
+++ assets/js/gulpfile.js
@@ -3,13 +3,13 @@ const sass = require("gulp-sass")(require("sass"));
 
 gulp.task("sass", function () {
   return gulp
-    .src("contents/**/*.scss")
+    .src("../../contents/**/*.scss")
     .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("assets/css"));
+    .pipe(gulp.dest("../css"));
 });
 
 gulp.task("watch", function () {
-  gulp.watch("contents/**/*.scss", gulp.series("sass"));
+  gulp.watch("../../contents/**/*.scss", gulp.series("sass"));
 });
 
 gulp.task("default", gulp.series("sass", "watch"));
